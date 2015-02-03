df5f93b144276672fd20af5150189fb9a93233de
anniemcghee <mcghee.annie@gmail.com>
2015-02-02 22:44:18 -0800

    Working on final result

--- js/app.js
+++ js/app.js
@@ -11,39 +11,49 @@ fruitsAndVeggies.controller('list',['$scope',function($scope){
   $scope.vegetables = vegetables;
   $scope.all = $scope.fruit.concat($scope.vegetables);
 
+  // $scope.all = $filter('orderBy')
+
 
   $scope.newFruit = [];
   $scope.newVeggie = [];
 
   $scope.addFruit = function(index) {
-    alert('going to Fruit!')
+    // alert('going to Fruit!')
     $scope.newFruit.push($scope.all[index]);
     $scope.all.splice(index,1);
     //set this on the arrow left inside fruits and vegs
   }
 
   $scope.addVeggie = function(index) {
-    alert('going to veggie!')
+    // alert('going to veggie!')
     $scope.newVeggie.push($scope.all[index]);
     $scope.all.splice(index,1);
     //set this on the right arrow inside fruits and vegs
   }
 
   $scope.addFromFruit = function(index) {
-    alert('going to all')
+    // alert('going to all')
     $scope.all.push($scope.newFruit[index]);
     $scope.newFruit.splice(index,1);
     //set this on the arrow in fruits and the arrow in vegs
   }
 
   $scope.addFromVeg = function(index) {
-    alert('going to all')
+    // alert('going to all')
     $scope.all.push($scope.newVeggie[index]);
     $scope.newVeggie.splice(index,1);
     //set this on the arrow in fruits and the arrow in vegs
   }
 
-// if newFruit = fruit && newVeggie = vegetables
+  $scope.checkWinner = function() {
+    alert('checking...');
+    // if (angular.equals(newFruit, fruit) && angular.equals(newVeggie, vegetables)) {
+    //   alert('You win!');
+    // } else {
+    //   alert('You lose!')
+    // }
+  }
+// = fruit && newVeggie = vegetables
 // you won!
 // else
 // highlight the ones that are mismatched
